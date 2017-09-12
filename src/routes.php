286ed719a93fa3b5cbb769a446e8e10a0f46b539
lvndry <monga.4landry@gmail.com>
2017-09-12 21:21:59 +0200

    ping system added

--- src/routes.php
+++ src/routes.php
@@ -1,35 +1,29 @@
 <?php
 
 // Routes
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
-
 $app->post('/create/user', function($request, $response, $args) {
   //Connexion to database
   try {
     $db = new db();
     $db = $db->connect();
+
+    //Creation of token
+    $adress = bin2hex(random_bytes(16));
+    echo $adress;
+
+    //Verify if the token doesn't already exists
+    $sql = 'SELECT COUNT(adresse) as count FROM users WHERE adresse ="'.$adress.'"';
+    $query = $db->query($sql);
+    $value = $query->fetch();
+
+    //If the token doesn't exist it is added to the database
+    if($value['count'] == 0){
+      $time = time();
+      $sql = 'INSERT INTO users(adresse, expire) VALUES ("'.$adress.'", "'.$time.'")';
+      $query = $db->exec($sql);
+    }
   } catch (Exception $e) {
       echo 'Error : '. $e->getMessage();
-  }
-  //Creation of token
-  $adress = bin2hex(random_bytes(16));
-  echo $adress;
-
-  //Verify if the token doesn't already exists
-  $sql = 'SELECT COUNT(adresse) as count FROM users WHERE adresse ="'.$adress.'"';
-  $query = $db->query($sql);
-  $value = $query->fetch();
-
-  //If the token doesn't exist it is added to the database
-  if($value['count'] == 0){
-    $sql = 'INSERT INTO users(adresse) VALUES ("'.$adress.'")';
-    $query = $db->exec($sql);
   }
 });
 
@@ -39,27 +33,28 @@ $app->post('/send/{id}/{message}', function($request, $response, $args) {
   try {
     $db = new db();
     $db = $db->connect();
+
+    $invalid_characters = array("$", "%", "#", "<", ">", "|", "\\");
+
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');
+    //Verify if the token doesn't already exists
+    $sql = 'SELECT COUNT(adresse) as count FROM users WHERE adresse ="'.$id.'"';
+    $query = $db->query($sql);
+    $value = $query->fetch();
+
+    //If the token doesn't exist it is added to the database
+    if($value['count'] != 0){
+      $message = $route->getArgument('message');
+      $message = str_replace($invalid_characters, "", $message);
+
+      //$sql = 'UPDATE users set message ="'.$message.'" WHERE adresse="'.$id.'" AND message is NULL';
+      $time = time();
+      $sql = 'INSERT INTO users(adresse, message, expire) VALUES ("'.$id.'", "'.$message.'", ".'$time.'")';
+      $query = $db->exec($sql);
+    }
   } catch (Exception $e) {
       echo 'Error : '. $e->getMessage();
-  }
-
-  $invalid_characters = array("$", "%", "#", "<", ">", "|", "\\");
-
-  $route = $request->getAttribute('route');
-  $id = $route->getArgument('id');
-  //Verify if the token doesn't already exists
-  $sql = 'SELECT COUNT(adresse) as count FROM users WHERE adresse ="'.$id.'"';
-  $query = $db->query($sql);
-  $value = $query->fetch();
-
-  //If the token doesn't exist it is added to the database
-  if($value['count'] != 0){
-    $message = $route->getArgument('message');
-    $message = str_replace($invalid_characters, "", $message);
-
-    //$sql = 'UPDATE users set message ="'.$message.'" WHERE adresse="'.$id.'" AND message is NULL';
-    $sql = 'INSERT INTO users(adresse, message) VALUES ("'.$id.'", "'.$message.'")';
-    $query = $db->exec($sql);
   }
 });
 
@@ -80,3 +75,21 @@ $app->get('/messages/{id}', function($request, $response, $args) {
       echo 'Error : '. $e->getMessage();
   }
 });
+
+$app->get('/{id}/status', function($request, $response, $args) {
+  $db = new db();
+  $db = $db->connect();
+
+  //Creation of token
+  $adress = bin2hex(random_bytes(16));
+  echo $adress;
+
+  //Verify if the token doesn't already exists
+  $sql = 'SELECT COUNT(expire) as expire FROM users WHERE adresse ="'.$adress.'"';
+  $query = $db->query($sql);
+  $token = $query->fetch();
+  if(time() - $token['expire'] > 30){
+    echo "offline";
+  }
+  else echo "online";
+})
